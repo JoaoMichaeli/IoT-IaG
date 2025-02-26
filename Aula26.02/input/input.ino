54e5c377b5673f97e6fa3efed077e28ced5e6763
João Victor Michaeli <161324070+JoaoMichaeli@users.noreply.github.com>
2025-02-26 09:01:44 -0300

    cod

---
+++ Aula26.02/input/input.ino
@@ -0,0 +1,47 @@
+// Atribuir nomes aos pinos
+
+// Botões
+#define BT1 8
+#define BT2 9
+
+// LEDs
+#define ledG 2
+#define ledR 4
+#define ledY 6
+
+void setup() {
+  // Configurar o sentido dos pinos de I/O
+  pinMode(BT1, INPUT);
+  pinMode(BT2, INPUT);
+
+  pinMode(ledG, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledY, OUTPUT);
+
+}
+
+void loop() {
+  if(digitalRead(BT1) == HIGH){
+    piscaGR();
+  }
+  else{
+    piscaY();
+  }
+}
+
+void piscaGR(){
+  digitalWrite(ledG, HIGH);
+  digitalWrite(ledR, LOW);
+  delay(250);
+  digitalWrite(ledR, HIGH);
+  digitalWrite(ledG, LOW);
+  delay(250);
+  digitalWrite(ledG, LOW);
+}
+
+void piscaY(){
+  digitalWrite(ledY, HIGH);
+  delay(350);
+  digitalWrite(ledY, LOW);
+  delay(350);
+}
