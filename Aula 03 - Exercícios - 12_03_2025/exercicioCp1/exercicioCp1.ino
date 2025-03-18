b40909a3a18a90e9c6bffee867e1cdfbc5e4c417
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-03-18 18:02:13 -0300

    att

---
+++ Aula 03 - Exercícios - 12_03_2025/exercicioCp1/exercicioCp1.ino
@@ -0,0 +1,25 @@
+// Questão 1
+#define Bt1 8
+#define Bt2 9
+#define Bt3 10
+#define LedRedV 2
+#define LedYellowV 3
+#define LedGreenV 4
+#define LedRedP 5
+#define LedGreenP 6
+
+
+void setup() {
+  pinMode(Bt1, INPUT_PULLUP);
+  pinMode(Bt2, INPUT_PULLUP);
+  pinMode(Bt3, INPUT_PULLUP);
+
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
