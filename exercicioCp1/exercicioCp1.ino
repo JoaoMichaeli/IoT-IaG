05b58765d53adc8d21c9c4369e4048423ff090b6
JoaoMichaeli <labsfiap@fiap.com.br>
2025-03-19 09:04:48 -0300

    cpSimulado

--- exercicioCp1/exercicioCp1.ino
+++ exercicioCp1/exercicioCp1.ino
@@ -1,3 +1,15 @@
+/*
+Sinal analógico: de 0.00 a 5.00v
+
+Conversor Analógico Digital (Analog to Digital Converter - ADC)
+(módulo dentro do chip ATMEGA328P)
+Converte o valor de tensão dos pinos de A0 a A5 em um valor
+decimal de 0 a 1023, por meio da função analogRead()
+*/
+
+// Variável que armazenará o valor convertido pelo ADC
+int valor;
+
 // Questão 1
 #define Bt1 8
 #define Bt2 9
@@ -10,16 +22,88 @@
 
 
 void setup() {
-  pinMode(Bt1, INPUT_PULLUP);
-  pinMode(Bt2, INPUT_PULLUP);
-  pinMode(Bt3, INPUT_PULLUP);
+  Serial.begin(9600); // Habilita comunicação serial do Arduino
+  // Baud Rate 9600 bits/s
 
-  
-  
+  pinMode(Bt1, INPUT);
+  pinMode(Bt2, INPUT);
+  pinMode(Bt3, INPUT);
 
+  pinMode(LedRedV, OUTPUT);
+  pinMode(LedYellowV , OUTPUT);
+  pinMode(LedGreenV , OUTPUT);
+  pinMode(LedRedP , OUTPUT);
+  pinMode(LedGreenP, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("RM555678");
 
+  fasesJuntas();
 }
+
+// Utilizando potenciometro
+void exemploPotenciometro(){
+  valor = analogRead(A2); // Ligação do potenciometro
+
+  if(valor > 950) digitalWrite(LedGreenP, HIGH);
+  else digitalWrite(LedGreenP, LOW);
+    
+  Serial.println(valor);
+  delay(1000);
+}
+
+// Questão 2
+void fase01(){
+  digitalWrite(LedGreenV, HIGH);
+  digitalWrite(LedRedP, HIGH);
+
+  delay(5000);
+
+  digitalWrite(LedGreenV, LOW);
+  digitalWrite(LedRedP, LOW);
+}
+
+void fase02(){
+  digitalWrite(LedYellowV, HIGH);
+  digitalWrite(LedRedP, HIGH);
+
+  delay(2000);
+
+  digitalWrite(LedYellowV, LOW);
+  digitalWrite(LedRedP, LOW);  
+}
+
+void fase03(){
+  digitalWrite(LedRedV, HIGH);
+  digitalWrite(LedGreenP, HIGH);
+
+  delay(5000);
+
+  digitalWrite(LedRedV, LOW);
+  digitalWrite(LedGreenP, LOW);
+}
+
+void fase04(){
+  digitalWrite(LedRedV, HIGH);
+  
+  for(int i=0; i<10; i++){
+    digitalWrite(LedRedP, HIGH);
+    delay(200);
+    digitalWrite(LedRedP, LOW);
+    delay(200);
+  }
+
+  delay(3000);
+
+  digitalWrite(LedRedV, LOW);
+}
+
+void fasesJuntas(){
+  fase01();
+  fase02();
+  fase03();
+  fase04();
+}
+
+// Questão 3
