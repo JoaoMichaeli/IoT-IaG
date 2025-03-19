551b4c7a1de57b53a8b798a0c2fec9e9292a7263
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-03-19 09:09:25 -0300

    cpSimulado

--- exercicioCp1/exercicioCp1.ino
+++ exercicioCp1/exercicioCp1.ino
@@ -10,7 +10,14 @@ decimal de 0 a 1023, por meio da função analogRead()
 // Variável que armazenará o valor convertido pelo ADC
 int valor;
 
-// Questão 1
+/*
+Questão 01 (0.50 ponto) – Configuração de dispositivos
+• Analise o diagrama proposto e utilize a diretiva #define para atribuir nomes aos pinos de I/O dos botões e dos 
+LEDs.
+• Utilize a função pinMode para configurar o sentido dos pinos (Utilize INPUT_PULLUP nos pinos dos botões).
+• Habilite o monitor Serial com velocidade de 9600 bits/s.
+• Escreva uma mensagem inicial no Monitor Serial, com os RMs dos integrantes do grupo.
+*/
 #define Bt1 8
 #define Bt2 9
 #define Bt3 10
@@ -53,7 +60,14 @@ void exemploPotenciometro(){
   delay(1000);
 }
 
-// Questão 2
+/*
+Questão 02 (2.50 pontos)
+Simule um semáforo, utilizando os cinco LEDs (três para veículos e dois para pedestres). 
+• Fase 01 (duração de 5 segundos): Verde para veículos. Vermelho para pedestres.
+• Fase 02 (duração de 2 segundos): Amarelo para veículos. Vermelho para pedestres.
+• Fase 03 (duração de 5 segundos): Vermelho para veículos. Verde para pedestres.
+• Fase 04 (duração de 3 segundos): Vermelho para veículos. Vermelho piscante para pedestres.
+*/
 void fase01(){
   digitalWrite(LedGreenV, HIGH);
   digitalWrite(LedRedP, HIGH);
@@ -106,4 +120,60 @@ void fasesJuntas(){
   fase04();
 }
 
-// Questão 3
+/*
+Questão 03 (2.00 pontos)
+Leia o valor da entrada analógica e armazene em uma variável "int". Se o valor lido estiver:
+• Abaixo de 300 ---> somente o LED verde da direita acende.
+• Maior ou igual a 300, porém menor que 600 ---> somente o LED amarelo acende.
+• Maior ou igual a 600, porém menor que 900 ---> somente o LED vermelho da direita acende.
+• Maior ou igual a 900, porém menor que 1000 ---> LEDs vermelho, amarelo e verde piscam.
+• Se o valor ultrapassar o 1000, os dois LEDs vermelhos piscam e o monitor Serial informa "Alarme!”
+• O sistema só voltará a funcionar quando o botão RESET for pressionado ou o sistema for reiniciado.
+• O monitor Serial deve apresentar o valor da variável monitorada.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Questão 04 (2.00 pontos)
+• Crie duas variáveis: limite_inf = 300 e limite_sup = 850
+• Leia o valor da entrada analógica e armazene em uma variável int, chamada "VALOR" (0 a 1023).
+• Condição 1 (inicial): Dois LEDs devem acender se VALOR ultrapassar o limite_sup.
+• Condição 2: Uma vez ultrapassado o limite_sup, os dois LEDs só apagarão se o limite_inf for atingido.
+• Condição 3: Os LEDs só voltam a acender, se o limite_sup for atingido novamente.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Questão 05 (3.00 pontos)
+• Quando o Arduino detectar que o estado do botão 1 passou de ligado para desligado, os dois LEDs verdes 
+devem acender.
+• Quando o Arduino detectar que o estado do botão 2 passou de ligado para desligado, os LEDs vermelhos 
+começam a piscar. Os verdes apagam.
+• Quando o Arduino detectar que o estado do botão 3 passou de ligado para desligado, os LEDs vermelhos 
+apagam. Ciclo reinicia.
+*/
